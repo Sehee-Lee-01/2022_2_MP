a8e5af4b322bdaef7fda291451477ba8b218d96c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-03 07:12:43 +0900

    feat: 세 번 째 레이아웃 일부 완성

--- app/src/main/java/com/cookandroid/project_testactivity/GoodsActivity.java
+++ app/src/main/java/com/cookandroid/project_testactivity/GoodsActivity.java
@@ -1,14 +1,24 @@
 package com.cookandroid.project_testactivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GoodsActivity extends AppCompatActivity {
     Button btnReturn;
+    ListView listGoods;
+    String[] data = {"dog", "cat", "pie", "rabbit", "5", "6", "7"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,40 @@ public class GoodsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        listGoods = (ListView) findViewById(R.id.listGoods);
+        ListAdapter adapter = new ListAdapter();
+        listGoods.setAdapter((adapter));
+
     }
 
+    class ListAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return data.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                view = inflater.inflate(R.layout.goods_row, null);
+            }
+            TextView sub_name = (TextView) view.findViewById(R.id.goodsName);
+            ImageView sub_image = (ImageView) view.findViewById(R.id.goodsImage);
+            CheckBox sub_check = (CheckBox) view.findViewById(R.id.goodsCheck);
+            sub_name.setText(data[i]);
+            sub_image.setImageResource(R.drawable.rabbit);
+            return view;
+        }
+    }
 }
